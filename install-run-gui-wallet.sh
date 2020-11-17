a9d598aee22358ac9e9af4e5ae6fcc5d2da483a7
brian <meshungina@gmail.com>
2020-11-17 23:42:51 +0200

    14th try

--- install-run-gui-wallet.sh
+++ install-run-gui-wallet.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 sudo apt update
-sudo apt upgrade
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
+sudo apt upgrade -y
 sudo apt install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev libzmq3-dev
 sudo apt install software-properties-common
 mkdir ~/dev
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz -P ~/dev/
 pushd ~/dev
 tar -xvf db-4.8.30.NC.tar.gz
-sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' ~/dev/db-4.8.30.NC/dbinc/atomic.h
+sed -i s/__atomic_compare_exchange/__atomic_compare_exchange_db/g ~/dev/db-4.8.30.NC/dbinc/atomic.h
 pushd ~/dev/db-4.8.30.NC/build_unix
 mkdir -p build
 BDB_PREFIX=/usr/local
 ../dist/configure --enable-cxx --prefix=$BDB_PREFIX
 make
 sudo make install
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev
-sudo apt-get install software-properties-common
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
 popd > /dev/null
 mkdir ~/Hydra
