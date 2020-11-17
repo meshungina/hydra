bca56a1f903872aec809e8a21401219a7b155395
brian <meshungina@gmail.com>
2020-11-17 16:53:56 +0200

    fourth try

--- install-run-gui-wallet.sh
+++ install-run-gui-wallet.sh
@@ -2,24 +2,24 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get --yes --force-yes install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
-sudo apt --yes --force-yes install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev libzmq3-dev
-sudo apt --yes --force-yes install software-properties-common
+sudo apt --yes install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev libzmq3-dev
+sudo apt --yes install software-properties-common
 mkdir ~/dev
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz -P ~/dev/
-tar -xvf ~/dev/db-4.8.30.NC.tar.gz
+pushd ~/dev
+tar -xvf db-4.8.30.NC.tar.gz
 sed -i s/__atomic_compare_exchange/__atomic_compare_exchange_db/g ~/dev/db-4.8.30.NC/dbinc/atomic.h
-pushd ~/db-4.8.30.NC/build_unix
+pushd ~/dev/db-4.8.30.NC/build_unix
 mkdir -p build
 BDB_PREFIX=/usr/local
 ../dist/configure --enable-cxx --prefix=$BDB_PREFIX
 make
 sudo make install
-sudo apt-get --yes --force-yes install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev
-sudo apt-get --yes --force-yes install software-properties-common
+sudo apt-get --yes install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev
+sudo apt-get --yes install software-properties-common
 sudo apt-get update -y
 sudo apt upgrade -y
-sudo apt-get --yes --force-yes install libdb4.8-dev libdb4.8++-dev
-sudo apt-get --yes --force-yes install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
+sudo apt-get --yes install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
 popd > /dev/null
 mkdir ~/Hydra
 pushd ~/Hydra
