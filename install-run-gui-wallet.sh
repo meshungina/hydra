9bd90303b1afb844624501e3748332ea1e565f61
brian <meshungina@gmail.com>
2020-11-18 09:38:49 +0200

    16th try

--- install-run-gui-wallet.sh
+++ install-run-gui-wallet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
-sudo apt install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev libzmq3-dev
-sudo apt install software-properties-common
+sudo apt -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev libzmq3-dev
+sudo apt -y install software-properties-common
 mkdir ~/dev
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz -P ~/dev/
 pushd ~/dev
@@ -14,7 +14,7 @@ BDB_PREFIX=/usr/local
 ../dist/configure --enable-cxx --prefix=$BDB_PREFIX
 make
 sudo make install
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
+sudo apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
 popd > /dev/null
 mkdir ~/Hydra
 pushd ~/Hydra
