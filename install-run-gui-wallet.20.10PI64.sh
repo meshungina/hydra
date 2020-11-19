3570933d2dd9c5637f03974106baf37161886005
brian <meshungina@gmail.com>
2020-11-19 19:41:03 +0200

    pi64ubuntu quick install

--- install-run-gui-wallet.20.10PI64.sh
+++ install-run-gui-wallet.20.10PI64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get autoremove --purge libdb-dev
+#sudo apt-get autoremove --purge libdb-dev
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev libzmq3-dev
