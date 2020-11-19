6a856c59d9b824610360ff336bfb6da3de583579
brian <meshungina@gmail.com>
2020-11-19 18:30:03 +0200

    added pi64 quick install

---
+++ install-run-gui-wallet.20.10PI64.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get autoremove --purge libdb-dev
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev libzmq3-dev
+sudo apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
+popd > /dev/null
+mkdir ~/Hydra
+pushd ~/Hydra
+wget https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra-0.1.0-aarch64-linux-gnu.zip
+unzip hydra-0.1.0-aarch64-linux-gnu.zip
+mkdir ~/.hydra
+wget https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra.conf -P ~/.hydra
+popd > /dev/null
+pushd ~/Hydra/bin/
+./hydra-qt -testnet
