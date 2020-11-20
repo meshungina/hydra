d569f2fbeb2a2e94d5d61415d7cda50c1d6988fc
brian <meshungina@gmail.com>
2020-11-20 13:56:05 +0200

    timestamp wget

--- install-run-gui-wallet.18.04.sh
+++ install-run-gui-wallet.18.04.sh
@@ -8,10 +8,10 @@ sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
 sudo apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
 mkdir ~/Hydra
 pushd ~/Hydra
-wget https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra-0.1.0-ubuntu18.04-x86_64-gnu.zip
+wget -N https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra-0.1.0-ubuntu18.04-x86_64-gnu.zip
 unzip hydra-0.1.0-ubuntu18.04-x86_64-gnu.zip
 mkdir ~/.hydra
-wget https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra.conf -P ~/.hydra
+wget -N https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra.conf -P ~/.hydra
 popd > /dev/null
 pushd ~/Hydra/bin/
 ./hydrad -daemon -testnet
