835113c4f3912b17cdced2ecab46f17e367f21c4
brian <meshungina@gmail.com>
2020-11-22 22:09:08 +0200

    sync all scripts

--- install-run-gui-wallet.20.04.sh
+++ install-run-gui-wallet.20.04.sh
@@ -34,7 +34,7 @@ sleep 10
 ./hydra-cli -version
 ./hydra-cli -testnet getinfo
 echo -e ""
-echo -e "Installation complete. HYDRA daemon started. Call the daemon with: \e[0;33m~/Hydra/bin/./hydra-cli -testnet getinfo\e[0m"
+echo -e "Installation complete. HYDRA daemon started in TESTNET. Call the daemon with: \e[0;33m~/Hydra/bin/./hydra-cli -testnet getinfo\e[0m"
 echo -e "You can stop the daemon with \e[0;33m~/Hydra/bin/./hydra-cli -testnet stop\e[0m and use the GUI instead with \e[0;33m~/Hydra/bin/./hydra-qt -testnet\e[0m"
 echo -e "Find more information at \e[0;33mhttps://hydrachain.org\e[0m"
 echo -e ""
