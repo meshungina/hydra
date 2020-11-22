b5bf15fb6b201113cfb2c636378a74694e50b5b6
brian <meshungina@gmail.com>
2020-11-22 21:13:23 +0200

    change repository binary update

--- install-run-gui-wallet.18.04.sh
+++ install-run-gui-wallet.18.04.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
 sudo apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler qrencode
 mkdir ~/Hydra
 pushd ~/Hydra
-wget -N https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra-0.1.0-ubuntu18.04-x86_64-gnu.zip
+wget -N https://github.com/Hydra-Chain/node/releases/download/hydra_v0.18.5/hydra-0.18.5-ubuntu18.04-x86_64-gnu.zip
 unzip hydra-0.1.0-ubuntu18.04-x86_64-gnu.zip
 mkdir ~/.hydra
 wget -N https://github.com/LockTrip/Blockchain/releases/download/hydra_testnet_v0.1.0/hydra.conf -P ~/.hydra
