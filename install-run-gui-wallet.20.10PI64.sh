4e47654ee694f0136c1c3a7fe3154769dcbcb5ef
brian <meshungina@gmail.com>
2020-12-02 11:33:33 +0200

    updated links set mainnet start

--- install-run-gui-wallet.20.10PI64.sh
+++ install-run-gui-wallet.20.10PI64.sh
@@ -10,19 +10,20 @@ sudo apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools
 popd > /dev/null
 mkdir ~/Hydra
 pushd ~/Hydra
-wget -N https://github.com/Hydra-Chain/node/releases/download/hydra_v0.18.5/hydra-0.18.5-aarch64-linux-gnu.zip
-unzip -o hydra-0.18.5-aarch64-linux-gnu.zip
+wget -N https://github.com/Hydra-Chain/node/releases/download/hydra_v0.18.5.1/hydra-0.18.5.1-arm-linux-gnueabihf.zip
+unzip -o hydra-0.18.5.1-arm-linux-gnueabihf.zip
 mkdir ~/.hydra
 cp ~/Hydra/hydra.conf ~/.hydra/
 popd > /dev/null
 pushd ~/Hydra/bin/
-echo -e "starting node in daemon mode"
-./hydrad -daemon -testnet -rescan -reindex
-sleep 5
+echo -e "starting node in daemon MainNet mode..."
+./hydrad -daemon -rescan -reindex
+sleep 15
 ./hydra-cli -version
-./hydra-cli -testnet getinfo
+sleep 20
+./hydra-cli getinfo
 echo -e ""
-echo -e "Installation complete. HYDRA daemon started in TESTNET. Call the daemon with: \e[0;33m~/Hydra/bin/./hydra-cli -testnet getinfo\e[0m"
-echo -e "You can stop the daemon with \e[0;33m~/Hydra/bin/./hydra-cli -testnet stop\e[0m and use the GUI instead with \e[0;33m~/Hydra/bin/./hydra-qt -testnet\e[0m"
+echo -e "Installation complete. HYDRA daemon started in MainNet. Call the daemon with: \e[0;33m~/Hydra/bin/./hydra-cli getinfo\e[0m"
+echo -e "You can stop the daemon with \e[0;33m~/Hydra/bin/./hydra-cli stop\e[0m and use the GUI instead with \e[0;33m~/Hydra/bin/./hydra-qt \e[0m"
 echo -e "Find more information at \e[0;33mhttps://hydrachain.org\e[0m"
 echo -e ""
